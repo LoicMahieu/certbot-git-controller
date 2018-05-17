e8d5527a205432d354ff866dbf3a4eade6909288
Loïc Mahieu <mahieuloic@gmail.com>
2018-05-17 18:29:59 +0200

    fix env

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 require('../lib/controller').start({
+  cronInterval: process.env.CRON_INTERNAL,
   domains: (process.env.DOMAINS || '').split(','),
   email: process.env.EMAIL,
   port: process.env.PORT,
+  renewTime: process.env.RENEW_TIME,
+  staging: false,
   staging: process.env.PRODUCTION !== 1,
   webroot: process.env.WEBROOT,
   gitRepository: process.env.GIT_REPOSITORY
