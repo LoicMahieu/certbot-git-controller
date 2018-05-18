49de39ff01841967da4884c35980600b5d714aec
Loïc Mahieu <mahieuloic@gmail.com>
2018-05-18 12:08:27 +0200

    Trim env values

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -2,12 +2,12 @@
 
 require('/app/lib/controller').start({
   cronInterval: process.env.CRON_INTERNAL,
-  domains: (process.env.DOMAINS || '').split(','),
+  domains: (process.env.DOMAINS || '').trim().split(','),
   email: process.env.EMAIL,
   initialCheckDelay: process.env.INITIAL_CHECK_DELAY,
   port: process.env.PORT,
   renewTime: process.env.RENEW_TIME,
-  staging: process.env.STAGING && process.env.STAGING !== "false",
+  staging: process.env.STAGING && (process.env.STAGING || '').trim() !== "false",
   webroot: process.env.WEBROOT,
   gitRepository: process.env.GIT_REPOSITORY
 })
