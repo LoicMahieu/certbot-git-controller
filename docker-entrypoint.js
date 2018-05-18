099d8b95cf80d0848fb5849f44dcabfd98f7854e
Loïc Mahieu <mahieuloic@gmail.com>
2018-05-18 15:33:41 +0200

    Store domains locally

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
-require('/app/lib/controller').start({
+
+const fs = require("fs");
+const path = require("path");
+const domains = fs.readFileSync(path.join(__dirname, "domains.txt"), "utf8")
+  .split("\n")
+  .map(v => v.trim())
+  .filter(Boolean);
+
+require("/app/lib/controller").start({
   cronInterval: process.env.CRON_INTERNAL,
-  domains: (process.env.DOMAINS || '').trim().split(','),
+  domains,
   email: process.env.EMAIL,
   initialCheckDelay: process.env.INITIAL_CHECK_DELAY,
   port: process.env.PORT,
   renewTime: process.env.RENEW_TIME,
-  staging: process.env.STAGING && (process.env.STAGING || '').trim() !== "false",
+  staging: process.env.STAGING && (process.env.STAGING || "").trim() !== "false",
   webroot: process.env.WEBROOT,
   gitRepository: process.env.GIT_REPOSITORY
 })
